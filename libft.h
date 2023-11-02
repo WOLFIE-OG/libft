c617a2049394473aedf98556f74e68b3d733abbd
WOLFIE_OG <35380754+WOLFIE-OG@users.noreply.github.com>
2023-11-02 18:52:26 +0000

    Update

--- libft.h
+++ libft.h
@@ -19,23 +19,26 @@
 # include <ctype.h>
 # include <bsd/string.h>
 
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_strlen(char *str);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+size_t		ft_strlcpy(char *dest, const char *src, size_t size);
+size_t		ft_strlcat(char *dest, char *src, size_t size);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+char		*ft_strchr(char *str, char c);
+int			ft_strncmp(char *s1, char *s2, size_t n);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
+int			ft_atoi(const char *c);
+
 char		*ft_strncpy(char *dest, char *src, size_t n);
 char		*ft_strcpy(char *dest, char *src);
-char		*ft_strchr(char *str, char c);
-char		*ft_strnstr(const char *big, const char *little, size_t len);
 char		*ft_strdup(char *src);
-size_t		ft_strlcat(char *dest, char *src, size_t size);
-size_t		ft_strlcpy(char *dest, const char *src, size_t size);
-int			ft_atoi(const char *c);
-int			ft_strlen(char *str);
-int			ft_strncmp(char *s1, char *s2, size_t n);
-int			ft_isalpha(int c);
-int			ft_isprint(int c);
-int			ft_isdigit(int c);
-int			ft_isascii(int c);
-int			ft_tolower(int c);
-int			ft_toupper(int c);
-void		*ft_memcpy(void *dest, const void *src, size_t n);
-void		*ft_memmove(void *dest, const void *src, size_t n);
-void		*ft_memset(void *s, int c, size_t n);
+
 #endif
