142668126c2838c7115c8c9bef98e312d8c96a90
WOLFIE_OG <oliverjtodd@yahoo.com>
2023-11-03 18:06:07 +0000

    Done bezero

--- libft.h
+++ libft.h
@@ -22,6 +22,7 @@ int			ft_isascii(int c);
 int			ft_isprint(int c);
 int			ft_strlen(char *str);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
+void	    *ft_bzero(void *s, size_t n);
 void		*ft_memset(void *s, int c, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 size_t		ft_strlcpy(char *dest, const char *src, size_t size);
