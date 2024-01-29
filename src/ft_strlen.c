0147b372d5aba00eb08fdee277c76e6a4510d6fb
WOLFIE_OG <oliverjtodd@yahoo.com>
2024-01-29 16:17:02 +0000

    Update

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,13 +6,15 @@
 /*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 17:35:59 by otodd             #+#    #+#             */
-/*   Updated: 2023/11/17 12:13:23 by otodd            ###   ########.fr       */
+/*   Updated: 2024/01/29 12:11:10 by otodd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(const char *s)
+#include "../include/libft.h"
+
+size_t	ft_strlen(const char *s)
 {
-	unsigned int	length;
+	size_t	length;
 
 	length = 0;
 	while (*s++)
