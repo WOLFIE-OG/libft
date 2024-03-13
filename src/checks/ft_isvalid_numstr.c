d25614442e1d1906d73913cabfad3f0bfce78fb6
WOLFIE_OG <oliverjtodd@yahoo.com>
2024-03-13 15:24:32 +0000

    Update

--- src/checks/ft_isvalid_numstr.c
+++ src/checks/ft_isvalid_numstr.c
@@ -6,7 +6,7 @@
 /*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 15:07:54 by otodd             #+#    #+#             */
-/*   Updated: 2024/03/13 15:17:51 by otodd            ###   ########.fr       */
+/*   Updated: 2024/03/13 15:24:08 by otodd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_isvalid_numstr(char *str)
 {
-	if (*str == '-')
+	if (*str == '-' && *(str + 1))
 		str++;
 	while (*str)
 		if (!ft_isdigit(*str++))
