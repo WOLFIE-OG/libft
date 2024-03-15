c70814f49b0eb683526a8fc771c200d4ee18a554
WOLFIE_OG <oliverjtodd@yahoo.com>
2024-03-15 13:15:45 +0000

    Update

--- src/utils/ft_file_extension.c
+++ src/utils/ft_file_extension.c
@@ -6,7 +6,7 @@
 /*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 18:10:31 by otodd             #+#    #+#             */
-/*   Updated: 2024/03/11 16:38:31 by otodd            ###   ########.fr       */
+/*   Updated: 2024/03/15 13:15:14 by otodd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ char	*ft_file_extension(char *str)
 {
 	char	**split;
 	char	*ext;
-
+	
+	if (!ft_strlen(str))
+		return (NULL);
 	split = ft_split(str, '.');
 	if (!split)
+		return (NULL);
+	if (!ft_strarraylen(split))
 		return (NULL);
 	ext = ft_strjoin(".", split[ft_strarraylen(split) - 1]);
 	if (!ext)
